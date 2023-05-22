92f5eb23912c88b9baabb53b2fbc65c4d1d8cc3c
Yigit Akoymak <akoymaky@gmail.com>
2023-05-22 12:46:45 +0100

    burasi

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 # Author :: Yiǧit Akoymak Date :: 19.05.2023
 # Basic social media program
+import time
+
+
 class SocialNetworkGraph:
     def __init__(self, file_name):
         self.file_name = file_name
         self.social_NW = {}
         self.pretty_display = ""
-        self.admin_name = ""
 
     def load_file(self):
         """Converts the social_NW file to a dictionary where first value is the key and the rest is value"""
@@ -32,9 +34,12 @@ class SocialNetworkGraph:
         return self.pretty_display
 
     def make_friend(self, frnd1, frnd2):
-        """Make two users friends in the database"""
-        self.social_NW[frnd1] = [frnd2]
-        self.social_NW[frnd2] = [frnd1]
+        """Make two users friends in the database if they are already in the database"""
+        if (frnd2 and frnd1) in self.social_NW.keys():
+            self.social_NW[frnd1] = [frnd2]
+            self.social_NW[frnd2] = [frnd1]
+            return f"{frnd1} and {frnd2} are now friends"
+        return f"{frnd2} or/and {frnd1} are not part of the database"
 
 
 class SocialNetwork(SocialNetworkGraph):
@@ -47,7 +52,7 @@ class SocialNetwork(SocialNetworkGraph):
 
     def lonely_users(self) -> str:
         """Returns users with no friend at all"""
-        lonely_list =[usr for usr in self.social_NW.keys() if len(self.social_NW[usr]) < 1]
+        lonely_list = [usr for usr in self.social_NW.keys() if len(self.social_NW[usr]) < 1]
         return f" {' '.join(lonely_list)} have/has no friend(s)"
 
     def less_friend(self) -> str:
@@ -69,61 +74,84 @@ class SocialNetwork(SocialNetworkGraph):
             relation_nw[key] = modified_values
         return f"User {member_id} has/have {appr} appearance(s) in our database\n{self.display_network(relation_nw)}"
 
-    def recommend_friend(self):
-        pass
+    def recommend_friend(self, reco_friend):
+        x = []
+        for friend in self.social_NW[reco_friend]:
+            y = set(self.social_NW[reco_friend]).difference(set(self.social_NW[friend]))
+            if "".join(y) not in self.social_NW[reco_friend]:
+                x.append(y)
+        print(x)
 
 
 class MainProgram(SocialNetwork, SocialNetworkGraph):
-    def main(self):
-        """User interactions | python 3.10 needed for this program to run"""
-        usr_inp = None
-        self.load_file()
-        print(f"{'_' * 10}SocialMediaManager{'_' * 10}\n"
-              f"| 0: exit from the program           |\n"
-              f"| 1: Load another the database       |\n"
-              f"| 2: Add user to the database        |\n"
-              f"| 3: Remove user from the database   |\n"
-              f"| 4: Show the user with no friends   |\n"
-              f"| 5: Show total friends of the user  |\n"
-              f"| 6: Display the database            |\n"
-              f"| 7: show users with less friends    |\n"
-              f"| 8: Relation diagram in database    |\n"
-              f"| 9: Recommend friend for the user   |\n"
-              f"| 10: Make users friend              |\n"
-              f"{'_' * 38}")
-        while usr_inp != "0":
-            usr_inp = input(f'{self.admin_name}@{self.file_name}$ ')
-            match usr_inp:
-                case "1":
-                    self.load_file()
-                case "2":
-                    add = input("please enter the user you want to add >")
-                    self.add_user(add)
-                    print(f"the user {add} has been added to the system")
-                case "3":
-                    rmv = input("please enter the user you want to remove >")
-                    self.remove_user(rmv)
-                    print(f'the user {rmv} has been removed')
-                case "4":
-                    print(self.lonely_users())
-                case "5":
-                    tfrnd = input("please enter the user name to see all friends >")
-                    print(f'{tfrnd} has/have {self.total_friends(tfrnd)} friends')
-                case "6":
-                    print(self.display_network(self.social_NW))
-                case "7":
-                    print(self.less_friend())
-                case "8":
-                    rel = input("please enter the user name to see the relationship in the system ")
-                    print(self.show_relations(rel))
-                case "9":
-                    pass
-                case "10":
-                    usr1 = input("please enter the first user name  > ")
-                    usr2 = input("please enter the second user name > ")
-                    self.make_friend(usr1, usr2)
+    def __init__(self, file_name, user_option):
+        super().__init__(file_name)
+        self.user_option = user_option
+        self.admin_name = None
+
+    @staticmethod
+    def intro():
+        """A text based command line interface"""
+        return str(f"{'_' * 10}SocialMediaManager{'_' * 10}\n"
+                   f"| 0: exit from the program           |\n"
+                   f"| 1: Load another the database       |\n"
+                   f"| 2: Add user to the database        |\n"
+                   f"| 3: Remove user from the database   |\n"
+                   f"| 4: Show the user with no friends   |\n"
+                   f"| 5: Show total friends of the user  |\n"
+                   f"| 6: Display the database            |\n"
+                   f"| 7: show users with less friends    |\n"
+                   f"| 8: Relation diagram in database    |\n"
+                   f"| 9: Recommend friend for the user   |\n"
+                   f"| 10: Make users friend              |\n"
+                   f"{'_' * 38}")
 
 
 if __name__ == '__main__':
-    x = MainProgram("social_nw.txt")  # Pass the file name or None if not applicable
-    x.main()
+    main = MainProgram(None, None)
+    main.admin_name = input("please enter the admin name > ")
+    main.file_name = input("please enter the file name > ")
+    main.load_file()
+    while main.user_option != "0":
+        try:
+            print(main.intro())
+            main.user_option = input(f'{main.admin_name}@{main.file_name}$ ')
+            match main.user_option:
+                case "1":
+                    db_file = input("please enter the database file > ")
+                    main = MainProgram(db_file, None)
+                    main.load_file()
+                case "2":
+                    add = input("please enter the user you want to add >")
+                    main.add_user(add)
+                    print(f"the user {add} has been added to the system")
+                case "3":
+                    rmv = input("please enter the user you want to remove >")
+                    main.remove_user(rmv)
+                    print(f'the user {rmv} has been removed')
+                case "4":
+                    print(main.lonely_users())
+                case "5":
+                    tfrnd = input("please enter the user name to see all friends >")
+                    print(f'{tfrnd} has/have {main.total_friends(tfrnd)} friends')
+                case "6":
+                    print(main.display_network(main.social_NW))
+                case "7":
+                    print(main.less_friend())
+                case "8":
+                    rel = input("please enter the user name to see the relationship in the system >")
+                    print(main.show_relations(rel))
+                case "9":
+                    r_friend = input("please enter the name of the user to recommend friend > ")
+                    main.recommend_friend(r_friend)
+                case "10":
+                    usr1 = input("please enter the first user name  > ")
+                    usr2 = input("please enter the second user name > ")
+                    print(main.make_friend(usr1, usr2))
+            time.sleep(2)
+        except (KeyError, FileNotFoundError) as errors:
+            print("""ERROR! Possible couses \n
+            1 - We try to access a key that does not exist in the dictionary.
+            2 - We try to open a file that does not exist. 
+                """)
+            time.sleep(2)
